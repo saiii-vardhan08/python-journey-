776e0f1c9f0eaf2ffcb5cdcf2f0e4418699df251
saiii-vardhan08 <kappa.saivardhan@gmail.com>
2026-03-31 23:02:57 +0530

    subtraction in python

---
+++ subtraction.py
@@ -0,0 +1,6 @@
+first_num = input("enter first number ")
+second_num = input("enter second number ")
+
+sum = float(first_num) - float(second_num)
+
+print("sum : " + str(sum)) # we converted sum to string because python dont know how to evaluate string with flaot like "sum" + 10.23
