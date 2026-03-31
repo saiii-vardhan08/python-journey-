ca48395ba272ef97225445fd6adb48e92d9c9fad
saivardhan <saivardhan200@outlook.com>
2026-03-31 22:34:23 +0530

    building strong fundamentals

---
+++ basics.py
@@ -0,0 +1,18 @@
+#printing
+print("hello world")
+
+#variables
+age1 = 20
+price = 19.64
+print(age1)
+print(price)
+
+
+#taking input
+name = input("enter your name ") #this returns the string
+print(name)
+
+# conversions 
+b_year = input("enter your birth year ")
+age = 2026 - int(b_year) # so input returns the string so subtraction is not possible so we have to convert into int
+print(age) # built in conversion functions int(), str(), bool(), float()
