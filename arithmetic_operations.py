0887788e1c0f733bc13a28849a8719bdfa124cbb
saiii-vardhan08 <kappa.saivardhan@gmail.com>
2026-04-01 22:27:57 +0530

    done with arithmetic operations

---
+++ arithmetic_operations.py
@@ -0,0 +1,20 @@
+#arithmetic operations
+
+print(10 + 3) # + is used for addition 
+print(10 - 3) # - is used for subtraction
+print(10 * 3) # * is used for multiplication
+print(10 / 3) # / is used for division
+
+print(10 // 3) # // is used for division and it returns int
+print(10 ** 3) # ** is used for power function
+print(10 % 3) # this is modulo operator used for remainder
+
+# now with variables
+
+x = 10
+x = x+3 # here we are incrementing x by 3 
+print(x)
+
+y = 10
+y+=3 # this is another method for writing y = y + 3
+print(y)
