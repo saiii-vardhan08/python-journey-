da63943d185dfbd0df38761bb4d2fe3dd455ac32
saiii-vardhan08 <kappa.saivardhan@gmail.com>
2026-04-01 22:01:24 +0530

    started basic strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+#basic string operations in python
+
+course = 'python for beginners'
+print(course.upper())
+print(course)
+
+name = "sai"
+
+print(name.upper())
+print(name.lower())
+print(name.find('a'))
