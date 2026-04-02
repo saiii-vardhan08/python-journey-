c03e9b9d0e2d4dbc352b197d6ec5a273e4d1fd42
saivardhan_goud_k <kappa.saivardhan@gmail.com>
2026-04-02 23:44:58 +0530

    Refactor addition.py to include user input
    Refactored addition logic to prompt for user input before calculating the sum.

--- addition.py
+++ addition.py
@@ -4,3 +4,10 @@ second_num = input("enter second number ")
 sum = float(first_num) + float(second_num)
 
 print("sum : " + str(sum)) # we converted sum to string because python dont know how to evaluate string with flaot like "sum" + 10.23
+
+first_num = float(input("enter first number "))
+second_num = float(input("enter second number "))
+
+sum = (first_num) + (second_num)
+
+print("sum : " + str(sum)) #this also gives the same but converted at starting
