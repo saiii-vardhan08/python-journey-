2d80fb5af9b210a6c8050ea39098ff22916c5ccd
saiii-vardhan08 <kappa.saivardhan@gmail.com>
2026-04-03 22:41:16 +0530

    comparision operators

---
+++ comparision_operators.py
@@ -0,0 +1,25 @@
+#comparision operators
+
+# means with that we can compare two entities
+# we have >, >=, <, <=, ==, !=
+
+x = 3 > 2 # this is the boolean expression means it tells weather x is true of false
+print(x)
+
+k = 3 >= 2
+print(k)
+
+a = 3 < 2
+print(a)
+
+b = 3 <= 2
+print(b)
+
+c = 3 == 3
+print(c) 
+
+d = 3 == 2
+print(d)
+
+e = 3 != 2
+print(e)
